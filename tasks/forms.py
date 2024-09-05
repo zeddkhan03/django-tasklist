96756fe70186999edf3b9cd8e98ee1cd54f8fc27
Mohd Zaid Khan <111868309+zeddkhan03@users.noreply.github.com>
2024-09-05 23:23:20 +0530

    Update forms.py

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Task
 
+# form to input task details in order to add the tasks.
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
