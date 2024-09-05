e0061482581184423ded1ccf39c12c9ffa42db7e
Mohd Zaid Khan <111868309+zeddkhan03@users.noreply.github.com>
2024-09-05 23:22:11 +0530

    Update urls.py

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# user-facing urls which go after ip & port
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
